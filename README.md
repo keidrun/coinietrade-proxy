93db76363c88d4f5b4f7178ab03aeed8976d2b66
keidrun <kssk96@gmail.com>
2018-10-13 17:40:48 -0700

    Add trevis settings

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # coinietrade-proxy
 
+[![Build Status][travis-image]][travis-url] [![Dependency Status][depstat-image]][depstat-url] [![License: MIT][license-image]][license-url]
+
 Keid's CoinieTrade project - Reverse proxy.
 
-## License
-
-* [No License](https://choosealicense.com/no-permission/)
-
-* This project is my own project
+[travis-url]: https://travis-ci.org/keidrun/coinietrade-proxy
+[travis-image]: https://secure.travis-ci.org/keidrun/coinietrade-proxy.svg?branch=master
+[depstat-url]: https://david-dm.org/keidrun/coinietrade-proxy
+[depstat-image]: https://david-dm.org/keidrun/coinietrade-proxy.svg
+[license-url]: https://opensource.org/licenses/GPL-3.0
+[license-image]: https://img.shields.io/badge/License-GPL3-yellow.svg
--- heroku-container-release.sh
+++ heroku-container-release.sh
@@ -6,14 +6,14 @@ if [ $# -ne 4 ]; then
   echo "1 - HEROKU_APP" 1>&2
   echo "2 - HEROKU_PROCESS_TYPE" 1>&2
   echo "3 - TAG (release version)" 1>&2
-  echo "4 - HEROKU_API_TOKEN" 1>&2
+  echo "4 - HEROKU_API_KEY" 1>&2
   exit 1
 fi
 
 readonly HEROKU_APP=$1
 readonly HEROKU_PROCESS_TYPE=$2
 readonly TAG=$3
-readonly HEROKU_API_TOKEN=$4
+readonly HEROKU_API_KEY=$4
 
 imageId=$(docker inspect registry.heroku.com/$HEROKU_APP/$HEROKU_PROCESS_TYPE:$TAG --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
@@ -21,4 +21,4 @@ curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP/formation \
 -d "$payload" \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
--H "Authorization: Bearer $HEROKU_API_TOKEN"
+-H "Authorization: Bearer $HEROKU_API_KEY"
